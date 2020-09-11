7e0bdbe98334c2d820dd2a4b274941e1070ec007
Muhammad Kumail <mkumailofficial@gmail.com>
2020-09-11 13:07:56 +0500

    Updates

---
+++ precache-manifest.91314b1f90416a5131d3861071429af8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7039e3f7dbb110a2373027f83be52f",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "4e200f5f1f52a605d4ce",
+    "url": "/react-movie-app/static/css/2.9e657d8f.chunk.css"
+  },
+  {
+    "revision": "d1d86a290c3c14e16f38",
+    "url": "/react-movie-app/static/css/main.907f149d.chunk.css"
+  },
+  {
+    "revision": "4e200f5f1f52a605d4ce",
+    "url": "/react-movie-app/static/js/2.674c442a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-app/static/js/2.674c442a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d86a290c3c14e16f38",
+    "url": "/react-movie-app/static/js/main.6b8eeec0.chunk.js"
+  },
+  {
+    "revision": "c6a5d50c70b0f3e9960d",
+    "url": "/react-movie-app/static/js/runtime-main.c668ab84.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-movie-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-movie-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-movie-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-movie-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-movie-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
